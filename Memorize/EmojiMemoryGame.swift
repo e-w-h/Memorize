fae0ea0cffcf300b014f7cea5933cce073702312
Eric Hou <erichou24@gmail.com>
2020-10-02 20:50:27 -0400

    created viewmodel

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Eric Hou on 10/2/20.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    // private(set) allows multiple views to look at but not change the model
+    // Closure - inlining a function in Swift
+    private var game: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory: { (pairIndex: Int) -> String in
+        return "😀"
+    })
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return game.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(card: MemoryGame<String>.Card) {
+        game.choose(card: card)
+    }
+}
