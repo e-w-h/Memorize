60fe78eddc9b9d8ceeb70ae0e05c2874730bfd49
Eric Hou <erichou24@gmail.com>
2020-10-05 18:09:23 -0400

    created grid layout to figure out the space to allocate each item within the view

---
+++ Memorize/GridLayout.swift
@@ -0,0 +1,26 @@
+//
+//  GridLayout.swift
+//  Memorize
+//
+//  Created by Eric Hou on 10/5/20.
+//
+
+import SwiftUI
+
+struct GridLayout{
+    var size: CGSize
+    var rowCount: Int = 0
+    var columnCount: Int = 0
+    
+    init(itemCount: Int, nearAspectRatio desiredAspectRatio: Double = 1, in size: CGSize) {
+        
+    }
+    
+    var itemSize: CGSize {
+        
+    }
+    
+    func location(ofItemAt index: Int) -> CGPoint {
+        
+    }
+}
