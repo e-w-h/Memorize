a315600fb64e3566d2b80b2c135449b825b7696b
Eric Hou <erichou24@gmail.com>
2020-10-05 18:03:08 -0400

    comments

--- Memorize/Grid.swift
+++ Memorize/Grid.swift
@@ -21,16 +21,20 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     var body: some View {
         // Figure out how much space has been allocated with the view
         GeometryReader { geometry in
+            // GeometryReader is escaping so we need self.body
             self.body(for: geometry.size)
         }
     }
     
+    // func so we can access self thats within scope
     func body(for size: CGSize) -> some View {
         ForEach(items) { item in
+            // ForEach is escaping so we need self.body
             self.body(for: item, in: size)
         }
     }
     
+    // func so we dont need self
     func body(for item: Item, in size: CGSize) -> some View {
         return viewForItem(item)
     }
