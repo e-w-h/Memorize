85e8407876ee167748df1339f8f65c946cde6aac
Eric Hou <erichou24@gmail.com>
2020-10-06 19:42:23 -0400

    init func body completed

--- Memorize/GridLayout.swift
+++ Memorize/GridLayout.swift
@@ -13,7 +13,24 @@ struct GridLayout{
     var columnCount: Int = 0
     
     init(itemCount: Int, nearAspectRatio desiredAspectRatio: Double = 1, in size: CGSize) {
-        
+        self.size = size
+        guard size.width != 0, size.height != 0, itemCount > 0 else { return }
+        var bestLayout: (rowCount: Int, columnCount: Int) = (1, itemCount)
+        var smallestVariance: Double?
+        let sizeAspectRatio = abs(Double(size.width/size.height))
+        for rows in 1...itemCount {
+            let columns = (itemCount / rows) + (itemCount % rows > 0 ? 1 : 0)
+            if (rows - 1) * columns < itemCount {
+                let itemAspectRatio = sizeAspectRatio * (Double(rows)/Double(columns))
+                let variance = abs(itemAspectRatio - desiredAspectRatio)
+                if smallestVariance == nil || variance < smallestVariance! {
+                    smallestVariance = variance
+                    bestLayout = (rowCount: rows, columnCount: columns)
+                }
+            }
+        }
+        rowCount = bestLayout.rowCount
+        columnCount = bestLayout.columnCount
     }
     
     var itemSize: CGSize {
