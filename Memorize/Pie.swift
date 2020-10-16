859225b7562e28bafbfbf88e69ee8c42a6d57d09
Eric Hou <erichou24@gmail.com>
2020-10-15 23:26:56 -0400

    connected two vars to the animation

--- Memorize/Pie.swift
+++ Memorize/Pie.swift
@@ -13,6 +13,17 @@ struct Pie: Shape {
     var endAngle: Angle
     var clockwise: Bool = false
     
+    // Required for animation
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
+    
     func path(in rect: CGRect) -> Path {
         // Designate a center for the path to move to
         let center = CGPoint(x: rect.midX, y: rect.midY)
